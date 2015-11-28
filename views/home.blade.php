90ea89b22a26b7d041b4e397cd6589ad61b15a20
Denkers <denkerssec@gmail.com>
2015-11-29 00:57:48 +0800

    $post > $domains DB connection Authentication removed "author by: " removed

--- views/home.blade.php
+++ views/home.blade.php
@@ -3,32 +3,61 @@
 {{$title}}
 @endsection
 @section('content')
-@if ( !$posts->count() )
+@if ( !$domains->count() )
 There is no e-mail domain!
 @else
 <div class="">
-  @foreach( $posts as $post )
+  @foreach( $domains as $domain )
   <div class="list-group">
+	<div class="table">
+			<?php
+				$servername = "localhost";
+				$username = "root";
+				$password = "root";
+				$dbname = "BEC";
+
+				// Create connection
+				$conn = new mysqli($servername, $username, $password, $dbname);
+				// Check connection
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				} 
+
+				$sql = "SELECT id, firstname, lastname FROM MyData";
+				$result = $conn->query($sql);
+
+				if ($result->num_rows > 0) {
+					echo "<table><tr><th>ID</th><th>Name</th></tr>";
+					// output data of each row
+					while($row = $result->fetch_assoc()) {
+						echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." ".$row["lastname"]."</td></tr>";
+					}
+					echo "</table>";
+				} else {
+					echo "0 results";
+				}
+				$conn->close();
+			?>
+		</div>
     <div class="list-group-item">
-      <h3><a href="{{ url('/'.$post->slug) }}">{{ $post->title }}</a>
-        @if(!Auth::guest() && ($post->author_id == Auth::user()->id || Auth::user()->is_admin()))
-          @if($post->active == '1')
-          <button class="btn" style="float: right"><a href="{{ url('edit/'.$post->slug)}}">Edit Post</a></button>
+      <h3><a href="{{ url('/'.$domains->slug) }}">{{ $domains->title }}</a>
+          @if($domain->active == '1')
+          <button class="btn" style="float: right"><a href="{{ url('edit/'.$domain->slug)}}">Edit Domain</a></button>
           @else
-          <button class="btn" style="float: right"><a href="{{ url('edit/'.$post->slug)}}">Edit Draft</a></button>
+          <button class="btn" style="float: right"><a href="{{ url('edit/'.$domain->slug)}}">Edit Draft</a></button>
           @endif
         @endif
       </h3>
-      <p>{{ $post->created_at->format('M d,Y \a\t h:i a') }} By <a href="{{ url('/user/'.$post->author_id)}}">{{ $post->author->name }}</a></p>
+      <p>{{ $domain->created_at->format('M d,Y \a\t h:i a') }}</a></p>
     </div>
     <div class="list-group-item">
       <article>
-        {!! str_limit($post->body, $limit = 1500, $end = '....... <a href='.url("/".$post->slug).'>Read More</a>') !!}
+        {!! str_limit($domain->body, $limit = 1500, $end = '....... <a href='.url("/".$domain->slug).'>Read More</a>') !!}
       </article>
     </div>
   </div>
   @endforeach
-  {!! $posts->render() !!}
+  {!! $domains->render() !!}
 </div>
 @endif
 @endsection
