da465814838df4ad25deb35fe7a29b54ac5355ca
Denkers <denkerssec@gmail.com>
2015-11-29 01:01:47 +0800

    $post > $domain "author by: " removed

--- views/posts/show.blade.php
+++ views/posts/show.blade.php
@@ -1,21 +1,20 @@
 @extends('app')
 @section('title')
-  @if($post)
-    {{ $post->title }}
-    @if(!Auth::guest() && ($post->author_id == Auth::user()->id || Auth::user()->is_admin()))
-      <button class="btn" style="float: right"><a href="{{ url('edit/'.$post->slug)}}">Edit Post</a></button>
+  @if($domain)
+    {{ $domain->title }}
+      <button class="btn" style="float: right"><a href="{{ url('edit/'.$domain->slug)}}">Edit domain</a></button>
     @endif
   @else
     Page does not exist
   @endif
 @endsection
 @section('title-meta')
-<p>{{ $post->created_at->format('M d,Y \a\t h:i a') }} By <a href="{{ url('/user/'.$post->author_id)}}">{{ $post->author->name }}</a></p>
+<p>{{ $domain->created_at->format('M d,Y \a\t h:i a') }}</a></p>
 @endsection
 @section('content')
-@if($post)
+@if($domain)
   <div>
-    {!! $post->body !!}
+    {!! $domain->body !!}
   </div>    
   <div>
     <h2>Leave a comment</h2>
@@ -24,14 +23,14 @@
     <p>Login to Comment</p>
   @else
     <div class="panel-body">
-      <form method="post" action="/comment/add">
+      <form method="domain" action="/comment/add">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <input type="hidden" name="on_post" value="{{ $post->id }}">
-        <input type="hidden" name="slug" value="{{ $post->slug }}">
+        <input type="hidden" name="on_domain" value="{{ $domain->id }}">
+        <input type="hidden" name="slug" value="{{ $domain->slug }}">
         <div class="form-group">
           <textarea required="required" placeholder="Enter comment here" name = "body" class="form-control"></textarea>
         </div>
-        <input type="submit" name='post_comment' class="btn btn-success" value = "Post"/>
+        <input type="submit" name='domain_comment' class="btn btn-success" value = "domain"/>
       </form>
     </div>
   @endif
