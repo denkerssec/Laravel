75c0607072a184170bd2300370f09bdc32b424ee
Denkers <denkerssec@gmail.com>
2015-12-04 00:04:08 +0800

    .

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('app')
+@section('title')
+{{$title}}
+@endsection
+@section('content')
+@if ( !$domains->count() )
+There is no e-mail domain!
+@else
+<div class="">
+  @foreach( $domains as $domain )
+  <div class="list-group">
+	<div class="table">
+	<h4>Email Domain</h4>
+		
+		
+		</div>
+    <div class="list-group-item">
+      <h3><a href="{{ url('/'.$domains->slug) }}">{{ $domains->title }}</a>
+          @if($domain->active == '1')
+          <button class="btn" style="float: right"><a href="{{ url('edit/'.$domain->slug)}}">Edit Domain</a></button>
+          @else
+          <button class="btn" style="float: right"><a href="{{ url('edit/'.$domain->slug)}}">Edit Draft</a></button>
+          @endif
+        
+      </h3>
+      <p>{{ $domain->created_at->format('M d,Y \a\t h:i a') }}</a></p>
+    </div>
+    <div class="list-group-item">
+      <article>
+        {!! str_limit($domain->body, $limit = 1500, $end = '....... <a href='.url("/".$domain->slug).'>Read More</a>') !!}
+      </article>
+    </div>
+  </div>
+  @endforeach
+  {!! $domains->render() !!}
+</div>
+@endif
+@endsection
